3fa773832251dab4b1e56e9f28eb216009741e22
SrijeshQA <srijeshqa@gmail.com>
2025-07-28 12:51:49 +0530

    new push

--- tests/newFile.test.ts
+++ tests/newFile.test.ts
@@ -1,10 +1,11 @@
 import test from "@playwright/test";
 
-
 test("New test #1", async ({page}) => {
     await page.goto("https://automationplayground.com/back-office/pages/login.html");
     await page.fill("//input[@name='email']","admin");
     await page.fill("//input[@name='password']","admin123");
     await page.click("//a[text()='Login']");
     await page.waitForLoadState();
+    const new1 = page.getByText("//div[div='New Comments!']/div[1]");
+    console.log("Value of " + new1);
 })
